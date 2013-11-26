3a3d25679d6f578a8a778abf8a5e5ef0fbe853aa
Thomas Dressler <thomas.dressler@colorado.edu>
2013-11-26 11:37:18 -0700

    Modify Migratons to work with Postgres

--- db/migrate/20131117012939_change_googleid_data_type.rb
+++ db/migrate/20131117012939_change_googleid_data_type.rb
@@ -1,5 +1,14 @@
 class ChangeGoogleidDataType < ActiveRecord::Migration
   def change
-	change_column :users, :google_id, :integer
+	add_column :users, :new_google_id, :integer
+
+    User.reset_column_information
+    User.all.each do |r|
+       r.new_google_id = r.google_id
+       r.save!
+    end
+
+    remove_column :users, :google_id
+    rename_column :users, :new_google_id, :google_id
   end
 end
